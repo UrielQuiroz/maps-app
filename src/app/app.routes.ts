b6ab3988f4f74e5f54a3e62a8d97f514fc962830
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-29 18:38:33 -0600

    Estructura del proyecto y definicion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullScreenMapPageComponent } from './pages/full-screen-map-page/full-screen-map-page';
+import { MarkersPageComponet } from './pages/markers-page/markers-page';
+import { HousesPageComponent } from './pages/houses-page/houses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullScreenMapPageComponent,
+    title: 'FullScreen Map'
+  },
+  {
+    path: 'markers',
+    component: MarkersPageComponet,
+    title: 'Markers'
+  },
+    {
+    path: 'houses',
+    component: HousesPageComponent,
+    title: 'Houses Available'
+  },
+    {
+    path: '**',
+    redirectTo: 'fullscreen'
+  }
+];
---
+++ src/app/pages/full-screen-map-page/full-screen-map-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-screen-map-page',
+  imports: [],
+  templateUrl: './full-screen-map-page.html',
+})
+export class FullScreenMapPageComponent { }
